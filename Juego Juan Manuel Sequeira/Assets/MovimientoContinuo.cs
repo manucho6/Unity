a93d2498d0bc97840396ce5d2d5858161ba245c1
Sequeira Juan Manuel <sequeirajuanmanuel@gmail.com>
2016-08-21 22:06:42 -0300

    TP 1
    Tp 1 Unity

---
+++ Juego Juan Manuel Sequeira/Assets/MovimientoContinuo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimientoContinuo : MonoBehaviour {
+    public float velocidadX;
+    public float velocidadY;
+    public float velocidadZ;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(velocidadX * Time.deltaTime, velocidadY * Time.deltaTime, velocidadZ* Time.deltaTime);
+    }
+}
---
+++ Juego Juan Manuel Sequeira/Assets/RotacionContinua.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotacionContinua : MonoBehaviour {
+    public float velocidadX;
+    public float velocidadY;
+    public float velocidadZ;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(velocidadX * Time.deltaTime, velocidadY * Time.deltaTime, velocidadZ * Time.deltaTime);
+    }
+}
